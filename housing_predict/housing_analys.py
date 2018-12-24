8f22ab3d81dfa4f80569ec8ca94d0c1b1675d773
hemiaolin <avalony1987@163.com>
2018-12-24 14:00:10 +0800

    创建测试集

--- housing_predict/housing_analys.py
+++ housing_predict/housing_analys.py
@@ -2,10 +2,12 @@ import os
 import tarfile
 from six.moves import urllib
 import pandas as pd
+import numpy as np
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
 HOUSING_PATH = "datasets/housing/"
 HOUSING_URL = DOWNLOAD_ROOT+HOUSING_PATH+"/housing.tgz"
+
 
 def fetch_housing_data(house_url=HOUSING_URL,housing_path=HOUSING_PATH):
     if not os.path.isdir(housing_path):
@@ -16,12 +18,21 @@ def fetch_housing_data(house_url=HOUSING_URL,housing_path=HOUSING_PATH):
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
 
+
 def load_housing_data(housing_path=HOUSING_PATH):
     """
         return DataFrame Pandas Obj
     """
     csv_path = os.path.join(housing_path,"housing.csv")
     return pd.read_csv(csv_path)
+
+
+def split_train_test(data,test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data)*test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices],data.iloc[test_indices]
 
 if __name__ == "__main__":
     fetch_housing_data()
